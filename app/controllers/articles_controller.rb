36146e3d40631eb23217fd4e249c2e1824eeed6b
helio.bitencourt <helio.bitencourt@pipefy.com>
2020-10-10 23:19:31 -0300

    atualizando

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
         
     def create
         #render plain: params[:article].inspect
