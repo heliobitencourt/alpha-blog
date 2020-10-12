6ee234eb09055c64554baad5966634a1eee7ea79
helio.bitencourt <helio.bitencourt@pipefy.com>
2020-10-12 19:23:56 -0300

    add one-to-many association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :usename, presence: true, 
                         uniqueness: { case_sensitivite: false }, 
                         length: { minimum: 3, maximum: 15 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, 
                         uniqueness: { case_sensitivite: false }, 
-                        length: { maximum: 100 }
+                        length: { maximum: 100 },
                         format: { with: VALID_EMAIL_REGEX }
 end
